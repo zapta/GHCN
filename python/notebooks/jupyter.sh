d34c9b665052179dadaef3453eb893b02b51bdc3
Zapta <zapta@users.noreply.github.com>
2023-09-04 20:51:23 -0700

    Added utility files.

---
+++ python/notebooks/jupyter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# A shell script to run Jupyter Notebook. This runs a local server and opens
+# a GUI in a web browser.
+#
+# Precondition:
+#   pip install jupyter
+
+jupyter notebook
+
